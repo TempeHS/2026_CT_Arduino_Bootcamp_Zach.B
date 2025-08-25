8296583fde71b082e1aa6d37fa3760e1dfe387c7
Zachary B <zachary.booth3@education.nsw.gov.au>
2025-08-25 12:47:57 +1000

    10.servo motor
    did some work w/ the servo and ultrasonic sensor

--- 10.servoMotor/10.servoMotor.ino
+++ 10.servoMotor/10.servoMotor.ino
@@ -22,12 +22,45 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/10.servoMotor/Bootcamp-servoMotor.png
 */
 
+// Includes for OLED Screen
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+// Includes for servo
 #include <Servo.h>
+// Includes for ultrasonic
+#include "Ultrasonic.h"
+
+
+
+
+unsigned static int servoPin = 6;
+unsigned static int usPin = 5;
+
+Servo myservo; // create servo object to control a servo
+Ultrasonic us_sensor(usPin);
+
+int potpin = A1; // analog pin used to connect the potentiometer
+int val; // variable to read the value from the analog pin
 
 void setup() {
-  
+  myservo.attach(servoPin);  // attaches the servo on pin 6 to the servo object
+  Serial.begin(9600);
+  Serial.print("Baud 9600");
+  Serial.print("---------");
 }
 
 void loop() {
-  
+  unsigned long RangeInCm;
+  RangeInCm = us_sensor.distanceRead();
+  RangeInCm = map(RangeInCm, 0, 357, 0, 180);
+  myservo.write(RangeInCm);
+  delay(100);
+
+
+  // val = analogRead(potpin);            // reads the value of the potentiometer
+  // val = map(val, 0, 1023, 180, 0);     // scale it to use with the servo (val)
+  // myservo.write(val);                  // sets the servo position according to the potentiometer
+  // delay(15);                           // waits for the servo to get there
 }
